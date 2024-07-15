7921d19992da10916ff12694241696bf93505131
alexandrecalonne <alexandre.calonne@epitech.eu>
2024-07-15 07:48:55 +0200

    init project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-red-100">
+        Hello world!
+      </h1>
+    </>
+  );
+}
+
+export default App;
